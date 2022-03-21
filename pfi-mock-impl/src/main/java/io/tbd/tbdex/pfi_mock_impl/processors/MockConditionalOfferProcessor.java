2760cb588b472ac1f31e8f8269ed7f265c91be21
Hellen Dou <hdou@squareup.com>
2022-03-21 15:51:42 -0700

    add processor module

---
+++ pfi-mock-impl/src/main/java/io/tbd/tbdex/pfi_mock_impl/processors/MockConditionalOfferProcessor.java
@@ -0,0 +1,10 @@
+package io.tbd.tbdex.pfi_mock_impl.processors;
+
+import io.tbd.tbdex.protocol.core.Message;
+import io.tbd.tbdex.protocol.processors.ConditionalOfferProcessor;
+
+public class MockConditionalOfferProcessor implements ConditionalOfferProcessor {
+  @Override public Message process(Message message) {
+    return null;
+  }
+}
---
+++ pfi-mock-impl/src/main/java/io/tbd/tbdex/pfi_mock_impl/processors/MockOfferAcceptProcessor.java
@@ -0,0 +1,10 @@
+package io.tbd.tbdex.pfi_mock_impl.processors;
+
+import io.tbd.tbdex.protocol.core.Message;
+import io.tbd.tbdex.protocol.processors.OfferAcceptProcessor;
+
+public class MockOfferAcceptProcessor implements OfferAcceptProcessor {
+  @Override public Message process(Message message) {
+    return null;
+  }
+}
