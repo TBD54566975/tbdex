d84be27c2f8e937ea8091c657a2d8f3b868c9626
Hellen Dou <hdou731@gmail.com>
2022-03-25 13:21:31 -0700

    add payment processor (#33)
    * add payment processor
    
    * add test

--- pfi-mock-impl/src/main/java/io/tbd/tbdex/pfi_mock_impl/circle/CircleClient.java
+++ pfi-mock-impl/src/main/java/io/tbd/tbdex/pfi_mock_impl/circle/CircleClient.java
@@ -1,163 +1,17 @@
 package io.tbd.tbdex.pfi_mock_impl.circle;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
-import org.json.JSONObject;
+import com.squareup.protos.tbd.pfi.BankAccount;
+import com.squareup.protos.tbd.pfi.CreateBankAccountRequest;
+import com.squareup.protos.tbd.pfi.CreateWirePaymentRequest;
+import com.squareup.protos.tbd.pfi.PayoutRequest;
+import com.squareup.protos.tbd.pfi.TransferRequest;
 
-public class CircleClient {
-  private static final Gson gson = new GsonBuilder()
-      .create();
-  private static final String BEARER_KEY = "";
+public interface CircleClient {
+  BankAccount createBankAccount(CreateBankAccountRequest createBankAccountRequest) throws Exception;
 
-  private final static Request.Builder requestBuilder = new Request.Builder()
-      .addHeader("Accept", "application/json")
-      .addHeader("Content-Type", "application/json")
-      .addHeader("Authorization", "Bearer " + BEARER_KEY);
+  void createWirePayment(CreateWirePaymentRequest createWirePaymentRequest) throws Exception;
 
-  public static BankAccount createBankAccount(CreateBankAccountRequest createBankAccountRequest)
-      throws Exception {
-    OkHttpClient client = new OkHttpClient();
+  void transfer(TransferRequest transferRequest) throws Exception;
 
-    MediaType mediaType = MediaType.parse("application/json");
-    RequestBody body = RequestBody.create(mediaType, gson
-        .toJson(createBankAccountRequest));
-
-    Request request = requestBuilder
-        .url("https://api-sandbox.circle.com/v1/banks/wires")
-        .post(body)
-        .build();
-
-    Response response = client.newCall(request).execute();
-    JSONObject data = new JSONObject(response.body().string()).getJSONObject("data");
-
-    BankAccount bankAccount = new BankAccount();
-    bankAccount.id = data.getString("id");
-    bankAccount.trackingRef = data.getString("trackingRef");
-    return bankAccount;
-  }
-
-  public static void createWirePayment(CreateWirePaymentRequest createWirePaymentRequest)
-      throws Exception {
-    OkHttpClient client = new OkHttpClient();
-
-    MediaType mediaType = MediaType.parse("application/json");
-    RequestBody body = RequestBody.create(mediaType, gson.toJson(createWirePaymentRequest));
-
-    Request request = requestBuilder
-        .url("https://api-sandbox.circle.com/v1/mocks/payments/wire")
-        .post(body)
-        .build();
-
-    Response response = client.newCall(request).execute();
-
-    System.out.println(response.body().string());
-  }
-
-  public static void transfer(TransferRequest transferRequest) throws Exception {
-    OkHttpClient client = new OkHttpClient();
-
-    MediaType mediaType = MediaType.parse("application/json");
-    RequestBody body = RequestBody.create(mediaType, gson.toJson(transferRequest));
-
-    Request request = requestBuilder
-        .url("https://api-sandbox.circle.com/v1/transfers")
-        .post(body)
-        .build();
-
-    Response response = client.newCall(request).execute();
-
-    System.out.println(response.body().string());
-  }
-
-  public static void payout(PayoutRequest payoutRequest) throws Exception {
-    OkHttpClient client = new OkHttpClient();
-
-    MediaType mediaType = MediaType.parse("application/json");
-    RequestBody body = RequestBody.create(mediaType, gson.toJson(payoutRequest));
-
-    Request request = requestBuilder
-        .url("https://api-sandbox.circle.com/v1/payouts")
-        .post(body)
-        .build();
-
-    Response response = client.newCall(request).execute();
-
-    System.out.println(response.body().string());
-  }
-
-  static class BankAccount {
-    String id;
-    String trackingRef;
-  }
-
-  static class CreateBankAccountRequest {
-    String accountNumber;
-    String routingNumber;
-    String idempotencyKey;
-    BankAddress bankAddress;
-    BillingDetails billingDetails;
-  }
-
-  static class BillingDetails {
-    String name ;
-    String city;
-    String country;
-    String line1;
-    String postalCode;
-    String district;
-  }
-
-  static class BankAddress {
-    String country;
-  }
-
-  static class CreateWirePaymentRequest {
-    Amount amount;
-    String trackingRef;
-  }
-
-  static class Amount {
-    String amount;
-    CurrencyCode currency;
-  }
-  enum CurrencyCode {
-    USD,
-    USDC
-  }
-
-  static class PayoutRequest {
-    Source source;
-    Destination destination;
-    Amount amount;
-    Metadata metadata;
-    String idempotencyKey;
-  }
-
-  static class TransferRequest {
-    Source source;
-    Destination destination;
-    Amount amount;
-    String idempotencyKey;
-  }
-
-  static class Source {
-    String id;
-    String type;
-  }
-
-  static class Destination {
-    String id;
-    String type;
-    String address;
-    String chain;
-  }
-
-  static class Metadata {
-    String beneficiaryEmail;
-  }
+  void payout(PayoutRequest payoutRequest) throws Exception;
 }
---
+++ pfi-mock-impl/src/test/java/io/tbd/tbdex/pfi_mock_impl/processors/PaymentProcessorTest.java
@@ -0,0 +1,34 @@
+package io.tbd.tbdex.pfi_mock_impl.processors;
+
+import com.squareup.protos.tbd.pfi.ConvertFundsRequest;
+import io.tbd.tbdex.pfi_mock_impl.TestBase;
+import io.tbd.tbdex.pfi_mock_impl.circle.MockCircleClient;
+import io.tbd.tbdex.protocol.core.Message;
+import io.tbd.tbdex.protocol.core.MessageThreadStore;
+import io.tbd.tbdex.protocol.messages.Ask;
+import java.math.BigDecimal;
+import javax.inject.Inject;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PaymentProcessorTest extends TestBase {
+  @Inject MessageThreadStore threadStore;
+
+  @Test
+  @DisplayName("runs without error")
+  void requestNotValid() {
+    PaymentProcessor paymentProcessor = new PaymentProcessor(new MockCircleClient());
+
+    String threadToken = "thid";
+    Message message1 = new Message.Builder("mid", threadToken, "pfi", "alice")
+        .build(new Ask("USDC", BigDecimal.valueOf(100), "USDC"));
+    threadStore.addMessageToThread(message1);
+
+    ConvertFundsRequest request = new ConvertFundsRequest.Builder()
+        .wallet_address("12345")
+        .thread_token(threadToken)
+        .build();
+
+    paymentProcessor.convertFunds(request);
+  }
+}
