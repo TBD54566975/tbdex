3d5b55841bb77b6f262cea38c53cde895dd96db1
Moe Jangda <moe@squareup.com>
2022-03-25 19:47:29 -0500

    jankfest begins

--- pfi-mock-impl/src/main/java/io/tbd/tbdex/pfi_mock_impl/store/HibernateUtil.java
+++ pfi-mock-impl/src/main/java/io/tbd/tbdex/pfi_mock_impl/store/HibernateUtil.java
@@ -19,7 +19,7 @@ public class HibernateUtil {
         // Hibernate settings equivalent to hibernate.cfg.xml's properties
         Properties settings = new Properties();
         settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
-        settings.put(Environment.URL, "jdbc:mysql://localhost:3307/tbdex");
+        settings.put(Environment.URL, "jdbc:mysql://mysql:3306/tbdex");
         settings.put(Environment.USER, "root");
         settings.put(Environment.PASS, "tbdev");
         settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
