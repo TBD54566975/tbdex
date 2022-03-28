94fd3d0e16db7ba6364f7bf0af7b7fb8ba191f2d
Hellen Dou <hdou@squareup.com>
2022-03-28 08:45:35 -0500

    move protos to lib

--- lib/src/main/java/io/tbd/tbdex/protocol/messages/OfferAccept.java
+++ lib/src/main/java/io/tbd/tbdex/protocol/messages/OfferAccept.java
@@ -1,10 +1,17 @@
 package io.tbd.tbdex.protocol.messages;
 
+import com.squareup.protos.tbd.pfi.PaymentProcessorRequest;
 import io.tbd.tbdex.protocol.core.MessageBody;
 import io.tbd.tbdex.protocol.core.MessageType;
 
 public class OfferAccept extends MessageBody {
-    public OfferAccept() {
+    public PaymentProcessorRequest paymentProcessorRequest;
+
+    public OfferAccept(PaymentProcessorRequest paymentProcessorRequest) {
         super(MessageType.OfferAccept);
+
+        this.paymentProcessorRequest = paymentProcessorRequest;
+
+        this.addValidReplyTypes(MessageType.Close);
     }
 }
