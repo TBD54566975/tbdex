0f29cf276659e3e7dd71f708ea751f6d988223c2
Mic Neale <michael.neale@gmail.com>
2023-02-17 11:49:07 +1100

    more cleanup

--- pfi-mock-impl/src/main/resources/migrations/v0001__messages.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS messages (
-  id           BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  thread_token VARCHAR(20) NOT NULL,
-  message      longtext NOT NULL,
-  created_at   TIMESTAMP NULL,
-  updated_at   TIMESTAMP NULL,
-  KEY idx_thread_token(thread_token)
-);
