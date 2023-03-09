e61992b8d7ef539f8cafed0aab7532438752df64
Mic Neale <michael.neale@gmail.com>
2023-03-09 16:02:09 +1100

    shifting to mocha and chai

--- protocol/jank-implementation/src/TBDexProtocol.test.ts
+++ protocol/jank-implementation/src/TBDexProtocol.test.ts
@@ -1,4 +1,5 @@
 import { RequestForQuoteMessage } from "./TBDexProtocol";
+import { expect } from "chai";
 
 describe("RequestForQuoteMessage", () => {
   const message: RequestForQuoteMessage = {
@@ -7,13 +8,13 @@ describe("RequestForQuoteMessage", () => {
     want: "Cheese",
     side: "have",
     size: 100,
-  };
+  }; 
 
   it("should have the correct properties", () => {
-    expect(message).toHaveProperty("id", "1234");
-    expect(message).toHaveProperty("have", "AUD");
-    expect(message).toHaveProperty("want", "Cheese");
-    expect(message).toHaveProperty("side", "have");
-    expect(message).toHaveProperty("size", 100);
+    expect(message).to.have.property("id", "1234");
+    expect(message).to.have.property("have", "AUD");
+    expect(message).to.have.property("want", "Cheese");
+    expect(message).to.have.property("side", "have");
+    expect(message).to.have.property("size", 100);
   });
 });
