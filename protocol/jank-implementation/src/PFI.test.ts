1b60af0f48c975b63d8d7358a25614648e924726
Mic Neale <michael.neale@gmail.com>
2023-03-10 18:41:03 +1100

    return a quote

--- protocol/jank-implementation/src/PFI.test.ts
+++ protocol/jank-implementation/src/PFI.test.ts
@@ -5,29 +5,43 @@ import { makeBid } from "./PFI";
 describe("Fetch Quotes", () => {
 
 
-    const rfq: RequestForQuote = {
-        id: '1234',
-        have: 'AUD',
-        want: 'Cheese',
-        side: 'have',
-        size: 100,
-      };
-    
-      const msg: TBDexMessage = {
+  it("smoke test", () => {
+    let msg: TBDexMessage = {
         id: 'abcd',
         contextId: 'efgh',
         from: 'did:example:alice',
         to: 'did:example:pfi',
         type: 'RequestForQuote',
-        body: rfq,
+        body: {
+            id: '1234',
+            have: 'AUD',
+            want: 'Cheese',
+            side: 'have',
+            size: 100,
+          },
         createdTime: Date.now(),
       };
-
-
-  it("should have the correct properties", () => {
     console.log(makeBid(msg));
-    // check that there are 3 quotes
-    const quotes = makeBid(msg);
-    expect(quotes).to.have.lengthOf(3);
+
+    msg = {
+        id: 'abcd',
+        contextId: 'efgh',
+        from: 'did:example:alice',
+        to: 'did:example:pfi',
+        type: 'RequestForQuote',
+        body: {
+            id: '1234',
+            have: 'AUD',
+            want: 'Cheese',
+            side: 'want',
+            size: 1,
+          },
+        createdTime: Date.now(),
+      };
+    let bid = makeBid(msg);    
+    console.log(bid);
+    console.log(bid[1].presentationDefinitionRequest);
+    
+
   });
 });
--- protocol/jank-implementation/src/PFI.ts
+++ protocol/jank-implementation/src/PFI.ts
@@ -1,71 +1,59 @@
 import { TBDexMessage, RequestForQuote, Quote } from "./TBDexProtocol";
 
 
-export function makeBid(msg: TBDexMessage): Quote[] {
-  const { from } = msg;
-  // Call a function to check the DID of the sender
-  const senderDid = checkDid(from);
+// Define a CSV lookup table of payment, product, rate, paymentType, and credentialsRequired
+const QUOTE_LOOKUP_TABLE = `
+AUD,USD,1.3,creditCard,driversLicense
+AUD,USD,1.4,wireTransfer,driversLicense
+AUD,USD,1.41,wireTransfer,passport
+AUD,Cheese,2,creditCard,none
+AUD,Cheese,1,creditCard,driversLicense
+`;
 
-  // Extract the RFQ from the message body
-  const rfq = msg.body as RequestForQuote;
-
-  // Create some quotes with different payment types
-  const quotes: Quote[] = [];
-
-  // Quote 1: Payment via wire transfer
-  const quote1: Quote = {
-    quoteId: generateQuoteId(),
-    requestForQuoteId: rfq.id,
-    offerUnit: rfq.side === 'have' ? rfq.have : rfq.want,
-    offerSize: rfq.size,
-    costUnit: rfq.side === 'have' ? rfq.want : rfq.have,
-    costSize: rfq.size * 1.05, // Add a 5% markup
-    paymentType: {
-      type: 'wire_transfer',
-    },
-    presentationDefinitionRequest: {},
+// Parse the CSV lookup table into an array of objects
+const QUOTES = QUOTE_LOOKUP_TABLE.trim().split('\n').map(line => {
+  const [payment, product, rate, paymentType, credentialsRequired] = line.split(',');
+  return {
+    payment,
+    product,
+    rate: parseFloat(rate),
+    paymentType,
+    credentialsRequired,
   };
-  quotes.push(quote1);
+});
 
-  // Quote 2: Payment via credit card
-  const quote2: Quote = {
-    quoteId: generateQuoteId(),
-    requestForQuoteId: rfq.id,
-    offerUnit: rfq.side === 'have' ? rfq.have : rfq.want,
-    offerSize: rfq.size,
-    costUnit: rfq.side === 'have' ? rfq.want : rfq.have,
-    costSize: rfq.size * 1.1, // Add a 10% markup
-    paymentType: {
-      type: 'credit_card',
-    },
-    presentationDefinitionRequest: {},
-  };
-  quotes.push(quote2);
+export function makeBid(message: TBDexMessage): Quote[] {
+  // Extract the RequestForQuote from the TBDexMessage body
+  const rfq = message.body as RequestForQuote;
 
-  // Quote 3: Payment via cryptocurrency
-  const quote3: Quote = {
-    quoteId: generateQuoteId(),
-    requestForQuoteId: rfq.id,
-    offerUnit: rfq.side === 'have' ? rfq.have : rfq.want,
-    offerSize: rfq.size,
-    costUnit: rfq.side === 'have' ? rfq.want : rfq.have,
-    costSize: rfq.size * 0.95, // Apply a 5% discount
-    paymentType: {
-      type: 'crypto',
-    },
-    presentationDefinitionRequest: {},
-  };
-  quotes.push(quote3);
+  // Filter the QUOTES array based on the RFQ criteria
+  const filteredQuotes = QUOTES.filter(q => q.payment === rfq.have && q.product === rfq.want);
+
+  // Create a Quote object for each matching quote
+  const quotes = filteredQuotes.map(q => {
+    let offerSize = 0, costSize = 0;
+
+    if (rfq.side === 'have') {
+      offerSize = rfq.size;
+      costSize = rfq.size / q.rate;
+    } else if (rfq.side === 'want') {
+      offerSize = rfq.size / q.rate;
+      costSize = rfq.size;
+    }
+
+    const quote: Quote = {
+      quoteId: Math.random().toString(36).substr(2, 9),
+      requestForQuoteId: message.id,
+      offerUnit: q.product,
+      offerSize,
+      costUnit: q.payment,
+      costSize,
+      paymentType: { type: q.paymentType },
+      presentationDefinitionRequest: { credentials: q.credentialsRequired === 'none' ? [] : [{ type: q.credentialsRequired }] },
+    };
+
+    return quote;
+  });
 
   return quotes;
-}
-
-function checkDid(from: string): string {
-  // TODO: Implement checking of DID based on sender information
-  return 'did:example:123';
-}
-
-function generateQuoteId(): string {
-  // TODO: Implement generation of unique quote IDs
-  return 'q123';
 }
