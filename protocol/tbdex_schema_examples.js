b2b4422ae59cad41fdf837a633c4629ebe969319
phoebe-lew <plew@squareup.com>
2023-06-20 09:33:18 +1000

    Implement json schema for tbDEX messages (#84)
    * add json schema
    * Readme and example schema updates

--- protocol/tbdex_schema_examples.js
+++ protocol/tbdex_schema_examples.js
@@ -1,15 +1,15 @@
 let offering = {
   "description": "Buy BTC with USD!",
   "pair": "BTC_USD",
-  "unitPrice": 27000.00,
-  "baseFee": 1.00,
-  "min": 10.00,
-  "max": 1000.00,
+  "unitPrice": "27000.00",
+  "baseFee": "1.00",
+  "min": "10.00",
+  "max": "1000.00",
   "presentationRequestJwt": "eyJhb...MIDw",
   "payinInstruments": [{
     "kind": "DEBIT_CARD",
     "fee": {
-      "flatFee": 1.00
+      "flatFee": "1.00"
     }
   }],
   "payoutInstruments": [{
@@ -19,7 +19,7 @@ let offering = {
 
 let rfq = {
   "pair": "BTC_USD",
-  "amount": 10.00,
+  "amount": "10.00",
   "verifiablePresentationJwt": "...",
   "payinInstrument": {
     "kind": "DEBIT_CARD"
@@ -31,8 +31,8 @@ let rfq = {
 
 let quote = {
   "expiryTime": "2023-04-14T12:12:12Z",
-  "totalFee": 2.00,
-  "amount": 0.000383,
+  "totalFee": "2.00",
+  "amount": "0.000383",
   "paymentPresentationRequestJwt": "eyJhbGc...EWfNnAw",
   "paymentInstructions": {
     "payin": {
