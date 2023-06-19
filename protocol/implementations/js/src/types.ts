63349785b875b5e348a1e86a1e3e14901342db6e
Phoebe Lew <plew@squareup.com>
2023-06-19 13:49:13 +1000

    scratch

--- protocol/implementations/js/src/types.ts
+++ protocol/implementations/js/src/types.ts
@@ -2,22 +2,22 @@ export namespace TbDex {
   export type Offering = {
     placeholder: string;
   }
-  
+
   export type RFQ = {
     test: string;
   }
-  
+
   export type Quote = {
     placeholder: string;
   }
-  
+
   export type MessageTypes = {
     rfq: RFQ,
     quote: Quote
   }
-  
+
   export type MessageType<M extends keyof MessageTypes> = MessageTypes[M];
-  
+
   export type Message<T extends keyof MessageTypes> = {
     id: string;
     contextId: string;
@@ -25,7 +25,44 @@ export namespace TbDex {
     to: string;
     type: T;
     body: MessageTypes[T];
-    dateCreated: string;
+    createdTime: number;
+  }
+
+  export function createWrapper(id: string, contextId: string, to: string, from: string) {
+    return {
+      createdTime: Date.now(),
+      id: id,
+      contextId: contextId,
+      from: from,
+      to: to,
+    }
+  }
+
+  export function wrapperFrom(message: Message<>, id: string) {
+    const {to, from, contextId} = message
+    return {
+      createdTime: Date.now(),
+      id: id,
+      to,
+      from,
+      contextId
+    }
+  }
+
+  export function createRfqMessage(rfq: RFQ): Message<"rfq"> {
+    return {
+      type: "rfq",
+      body: rfq,
+      ...createWrapper()
+    }
+  }
+
+  export function createQuoteMessage(quote: Quote): Message<"quote"> {
+    return {
+      type: "rfq",
+      body: rfq,
+      ...wrapperFrom()
+    }
   }
 }
 
