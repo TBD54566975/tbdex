792e2c4861dbc26900ed2f626121dba85819005e
Phoebe Lew <plew@squareup.com>
2023-06-20 11:40:08 +1000

    more wip

---
+++ protocol/implementations/js/src/utils/validator.ts
@@ -0,0 +1,29 @@
+import Ajv from 'ajv';
+
+import tbdexMessage from '../../../../json-schemas/tbdex-message.schema.json' assert { type: 'json' };
+
+const validator = new Ajv();
+validator.addSchema(tbdexMessage, 'tbdex-message');
+
+export const SchemaName = 'tbdex-message';
+
+export function validateMessage(payload: any): void {
+    const validateFn = validator.getSchema(SchemaName);
+
+    if (!validateFn) {
+        throw new Error(`schema for ${SchemaName} not found.`);
+    }
+
+    validateFn(payload);
+
+    if (!validateFn.errors) {
+        return;
+    }
+
+    // AJV is configured by default to stop validating after the 1st error is encountered which means
+    // there will only ever be one error;
+    const [errorObj] = validateFn.errors;
+    let { instancePath, message } = errorObj;
+
+    throw new Error(`${instancePath ?? SchemaName}: ${message}`);
+}
--- protocol/implementations/js/src/validator.ts
+++
@@ -1,9 +0,0 @@
-import Ajv from 'ajv';
-
-import JsonRpcRequest from '../../../json-schemas/tbdex-message-schema.json' assert { type: 'json' };
-
-const validator = new Ajv.default();
-validator.addSchema(JsonRpcRequest, 'jsonrpc-request');
-validator.addSchema(dwnRequest, 'dwn-request');
-
-export type SchemaNames = 'jsonrpc-request' | 'dwn-request';
