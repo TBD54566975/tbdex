3074dd27d27a86701febaf0f53ae1b9ed1a80f80
Moe Jangda <moe@tbd.email>
2023-06-21 17:01:33 -0500

    setup tests and finish out build & bundling setup

--- protocol/implementations/js/build/bundles.js
+++ protocol/implementations/js/build/bundles.js
@@ -1,30 +1,6 @@
 import esbuild from 'esbuild';
 import browserConfig from './esbuild-browser-config.cjs';
 
-// cjs bundle. external dependencies **not** bundled
-esbuild.buildSync({
-  platform       : 'node',
-  bundle         : true,
-  format         : 'cjs',
-  packages       : 'external',
-  sourcemap      : true,
-  entryPoints    : ['./src/main.ts'],
-  outfile        : './dist/cjs/main.cjs',
-  allowOverwrite : true,
-});
-
-// esm bundle. external dependencies **not** bundled
-esbuild.buildSync({
-  platform       : 'node',
-  bundle         : true,
-  format         : 'esm',
-  packages       : 'external',
-  sourcemap      : true,
-  entryPoints    : ['./src/main.ts'],
-  outfile        : './dist/esm/main.mjs',
-  allowOverwrite : true,
-});
-
 // esm polyfilled bundle for browser
 esbuild.build({
   ...browserConfig,
