3804d7abcf47c71763959efbde627648382f25ad
Phoebe Lew <plew@squareup.com>
2023-06-21 10:48:17 +1000

    Delete incorrect js files

--- protocol/implementations/js/src/main.js
+++
@@ -1,3 +0,0 @@
-export * from './types.js';
-export * from './builders.js';
-export * from './validator.js';
--- protocol/implementations/js/tests/builders.spec.js
+++
@@ -1,6 +0,0 @@
-import { expect } from 'chai';
-describe('Example Test Suite', () => {
-    it('needs tests', () => {
-        expect(true).to.be.true;
-    });
-});
