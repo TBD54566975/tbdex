11a1c1bf41680ab10f7270575262c8f1ce502589
Jiyoon Koo <jiyoon@squareup.com>
2023-06-22 20:51:29 -0700

    formatting changes

--- protocol/implementations/js/src/protocolDefinitions.ts
+++ protocol/implementations/js/src/protocolDefinitions.ts
@@ -1,54 +1,54 @@
 export const aliceProtocolDefinition = {
-    'protocol': 'https://tbd.website/protocols/tbdex',
-    'types': {
-        'RFQ': {
-            'schema': 'https://tbd.website/protocols/tbdex/RequestForQuote',
-            'dataFormats': [
+    protocol: 'https://tbd.website/protocols/tbdex',
+    types: {
+        RFQ: {
+            schema: 'https://tbd.website/protocols/tbdex/RequestForQuote',
+            dataFormats: [
                 'application/json'
             ]
         },
-        'Quote': {
-            'schema': 'https://tbd.website/protocols/tbdex/Quote',
-            'dataFormats': [
+        Quote: {
+            schema: 'https://tbd.website/protocols/tbdex/Quote',
+            dataFormats: [
                 'application/json'
             ]
         },
-        'Order': {
-            'schema': 'https://tbd.website/protocols/tbdex/Order',
-            'dataFormats': [
+        Order: {
+            schema: 'https://tbd.website/protocols/tbdex/Order',
+            dataFormats: [
                 'application/json'
             ]
         },
-        'OrderStatus': {
-            'schema': 'https://tbd.website/protocols/tbdex/OrderStatus',
-            'dataFormats': [
+        OrderStatus: {
+            schema: 'https://tbd.website/protocols/tbdex/OrderStatus',
+            dataFormats: [
                 'application/json'
             ]
         }
     },
-    'structure': {
+    structure: {
         // alice sends RFQs, not receives them
-        'RFQ': {
-            '$actions': [],
+        RFQ: {
+            $actions: [],
             // whoever received the RFQ that Alice sent, can write back a Quote to Alice
-            'Quote': {
-                '$actions': [
+            Quote: {
+                $actions: [
                     {
-                        'who': 'recipient',
-                        'of': 'RFQ',
-                        'can': 'write'
+                        who: 'recipient',
+                        of: 'RFQ',
+                        can: 'write'
                     }
                 ],
                 // alice sends Orders, not receives them
-                'Order': {
-                    '$actions': [],
+                Order: {
+                    $actions: [],
                     // whoever received the order that Alice sent in response to a Quote in response to an RFQ, can write back an OrderStatus to Alice.
-                    'OrderStatus': {
-                        '$actions': [
+                    OrderStatus: {
+                        $actions: [
                             {
-                                'who': 'recipient',
-                                'of': 'RFQ/Quote/Order',
-                                'can': 'write'
+                                who: 'recipient',
+                                of: 'RFQ/Quote/Order',
+                                can: 'write'
                             }
                         ]
                     }
@@ -60,59 +60,59 @@ export const aliceProtocolDefinition = {
 
 
 export const pfiProtocolDefinition = {
-    'protocol': 'https://tbd.website/protocols/tbdex',
-    'types': {
-        'RFQ': {
-            'schema': 'https://tbd.website/protocols/tbdex/RequestForQuote',
-            'dataFormats': [
+    protocol: 'https://tbd.website/protocols/tbdex',
+    types: {
+        RFQ: {
+            schema: 'https://tbd.website/protocols/tbdex/RequestForQuote',
+            dataFormats: [
                 'application/json'
             ]
         },
-        'Quote': {
-            'schema': 'https://tbd.website/protocols/tbdex/Quote',
-            'dataFormats': [
+        Quote: {
+            schema: 'https://tbd.website/protocols/tbdex/Quote',
+            dataFormats: [
                 'application/json'
             ]
         },
-        'Order': {
-            'schema': 'https://tbd.website/protocols/tbdex/Order',
-            'dataFormats': [
+        Order: {
+            schema: 'https://tbd.website/protocols/tbdex/Order',
+            dataFormats: [
                 'application/json'
             ]
         },
-        'OrderStatus': {
-            'schema': 'https://tbd.website/protocols/tbdex/OrderStatus',
-            'dataFormats': [
+        OrderStatus: {
+            schema: 'https://tbd.website/protocols/tbdex/OrderStatus',
+            dataFormats: [
                 'application/json'
             ]
         }
     },
-    'structure': {
-        // anyone can write RFQ to a PFI's DWN
-        // no one can read RFQs from PFI's DWN (except the PFI itself)
-        'RFQ': {
-            '$actions': [
+    structure: {
+        // anyone can write RFQ to a PFIs DWN
+        // no one can read RFQs from PFIs DWN (except the PFI itself)
+        RFQ: {
+            $actions: [
                 {
-                    'who': 'anyone',
-                    'can': 'write'
+                    who: 'anyone',
+                    can: 'write'
                 }
             ],
             // PFI is sending OUT quotes. no one should be writing Quotes to PFIs.
-            'Quote': {
-                '$actions': [],
-                // only Alice, who received an RFQ/Quote, can write Order to PFI's DWN
-                // no one can read Order from PFI's DWN (except the PFI itself)
-                'Order': {
-                    '$actions': [
+            Quote: {
+                $actions: [],
+                // only Alice, who received an RFQ/Quote, can write Order to PFIs DWN
+                // no one can read Order from PFIs DWN (except the PFI itself)
+                Order: {
+                    $actions: [
                         {
-                            'who': 'recipient',
-                            'of': 'RFQ/Quote',
-                            'can': 'write'
+                            who: 'recipient',
+                            of: 'RFQ/Quote',
+                            can: 'write'
                         }
                     ],
                     // PFI is sending OUT OrderStatus. no one should be writing OrderStatus to PFIs.
-                    'OrderStatus': {
-                        '$actions': []
+                    OrderStatus: {
+                        $actions: []
                     }
                 }
             }
