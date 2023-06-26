c0ddd0e9f75ed7bd1ba36d431c088f9089f9027f
Phoebe Lew <plew@squareup.com>
2023-06-26 15:05:34 +1000

    Refactor builder impl and improve tests

--- protocol/implementations/js/src/builders.ts
+++ protocol/implementations/js/src/builders.ts
@@ -1,65 +1,42 @@
-import { MessageType, MessageTypes, Metadata, TbDEXMessage } from './types.js'
+import { MessageType, MessageTypes, TbDEXMessage } from './types.js'
 import {ulid} from 'ulidx'
+
+type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: never }
+type XOR<T, U> = (T | U) extends object ? (Without<T, U> & U) | (Without<U, T> & T) : T | U
+
+type NewThreadOpts = {
+  from: string,
+  to: string,
+}
+
+type LastMessageOpts<T extends keyof MessageTypes> = {
+  last: TbDEXMessage<T>
+}
+
+export type CreateMessageOpts<T extends keyof MessageTypes, U extends keyof MessageTypes> = XOR<NewThreadOpts, LastMessageOpts<U>> & {
+  type: T,
+  body: MessageType<T>,
+}
+
+export function createMessage<T extends keyof MessageTypes, U extends keyof MessageTypes>(opts: CreateMessageOpts<T, U>): TbDEXMessage<T> {
+  const id = createId(opts.type)
+  const contextId = opts.last?.contextId ?? id
+  const {type, body} = opts
+
+  return {
+    id,
+    contextId,
+    createdTime : new Date().toISOString(),
+    type,
+    body,
+    to          : opts.to ?? opts.last.to,
+    from        : opts.from ?? opts.last.from,
+  }
+}
 
 // maybe extract this somewhere?
 const ID_NAMESPACE = 'tbdex'
 
-function createId(messageType: string): string {
+function createId<T extends keyof MessageTypes>(messageType: T): string {
   return `${ID_NAMESPACE}:${messageType}:${ulid()}`
-}
-
-export function createMessage<Type extends keyof MessageTypes>(to: string, from: string, body: MessageTypes[Type], messageType: Type,): TbDEXMessage<Type> {
-  // TODO could validate that type is Offering or RFQ, since they start chains or are one off (no replies)
-  const id = createId(messageType)
-  return {
-    id          : id,
-    contextId   : id,
-    createdTime : new Date().toISOString(),
-    to          : to,
-    from        : from,
-    type        : messageType,
-    body        : body,
-  }
-}
-
-export function createMessageFrom<Type extends keyof MessageTypes, LastType extends keyof MessageTypes>(body: MessageTypes[Type], messageType: Type, lastMessage: TbDEXMessage<LastType>): TbDEXMessage<Type> {
-  const { to, from, contextId } = lastMessage
-  return {
-    id          : createId(messageType),
-    contextId   : contextId,
-    createdTime : new Date().toISOString(),
-    to          : to,
-    from        : from,
-    type        : messageType,
-    body        : body,
-  }
-}
-
-
-export type CreateMessageOpts<T extends keyof MessageTypes> = Partial<Metadata> & {
-  from: string,
-  to: string,
-  messageType: T,
-  body: MessageType<T>,
-}
-
-export function createMessage2<T extends keyof MessageTypes>(opts: CreateMessageOpts<T>): TbDEXMessage<T> {
-  if (messageType )
-  const {to, from, messageType, body} = opts
-  const id = opts.id ?? createId(opts.messageType)
-
-  var contextId 
-  if( !opts.contextId && messageType in ['rfq', 'offering']) {
-    contextId = id
-  }
-  return {
-    id          : opts.id ?? createId(opts.messageType),
-    contextId   : opts.contextId ?? ,
-    createdTime : new Date().toISOString(),
-    type : messageType        ,
-    to          ,
-    from        ,
-    body        ,
-  }
-  console.log(opts)
 }
--- protocol/implementations/js/tests/builders.spec.ts
+++ protocol/implementations/js/tests/builders.spec.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai'
-import { Offering } from '../src/types.js'
-import { createMessage, createMessage2 } from '../src/builders.js'
+import { Offering, Quote, Rfq, PaymentInstrumentKind } from '../src/types.js'
+import { createMessage } from '../src/builders.js'
 
 describe('Message builder', () => {
   it('can build an offering', () => {
@@ -15,16 +15,51 @@ describe('Message builder', () => {
       payoutInstruments      : []
     }
 
-    const tbdexMessage = createMessage2({
-      'messageType' : 'offering',
-      from          : 'did1',
-      to            : 'did2',
-      body          : offering,
+    const actual = createMessage({
+      to   : 'alice-did',
+      from : 'pfi-did',
+      type : 'offering',
+      body : offering
     })
 
-    console.log(tbdexMessage)
+    expect(actual.body).to.equal(offering)
+  })
+  it('builds the expected message for an existing thread', () => {
+    const rfq: Rfq = {
+      pair                      : 'USD_BTC',
+      amount                    : '1000',
+      verifiablePresentationJwt : 'fake-jwt',
+      payinInstrument           : {
+        kind: PaymentInstrumentKind.DEBIT_CARD
+      },
+      payoutInstrument: {
+        kind: PaymentInstrumentKind.BITCOIN_ADDRESS
+      }
+    }
 
-    console.log(createMessage('alice-did', 'pfi-did', offering, 'offering'))
-    expect(createMessage('alice-did', 'pfi-did', offering, 'offering')).to.not.throw
+    const rfqMessage = createMessage({
+      to   : 'pfi-did',
+      from : 'alice-did',
+      type : 'rfq',
+      body : rfq
+    })
+
+    const quote: Quote = {
+      expiryTime                    : new Date().toISOString(),
+      totalFee                      : '100',
+      amount                        : '1000',
+      paymentPresentationRequestJwt : '',
+      paymentInstructions           : {payin: {link: 'fake.link.com'}}
+    }
+
+    const {from, to, contextId} = createMessage({
+      last : rfqMessage,
+      type : 'quote',
+      body : quote
+    })
+
+    expect(from).to.equal(rfqMessage.from)
+    expect(to).to.equal(rfqMessage.to)
+    expect(contextId).to.equal(rfqMessage.contextId)
   })
 })
