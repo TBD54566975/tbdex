f81cffd0443f2aec99bcaee5a35928971f8d7ecd
Phoebe Lew <plew@squareup.com>
2023-07-18 20:29:28 +1000

    Add alternate implementation, doesnt entirely enforce args correctly with close

--- js/src/builders.ts
+++ js/src/builders.ts
@@ -6,17 +6,82 @@ type NewThreadOpts = {
   to: string,
 }
 
-type LastMessageOpts<T extends keyof MessageTypes> = {
-  last: TbDEXMessage<T>
-}
-
 type ReplyMessageOpts<T extends keyof MessageTypes> = {
   reply: TbDEXMessage<T>
 }
 
-export type CreateMessageOpts<T extends keyof MessageTypes, U extends keyof MessageTypes> = XOR<NewThreadOpts, LastMessageOpts<U>> & {
+export type CreateMessageOpts<T extends keyof MessageTypes, U extends keyof MessageTypes> = {
+  to: string,
+  from: string,
   type: T,
   body: MessageType<T>,
+  last?: TbDEXMessage<U>
+}
+
+export function createMessageDumb<T extends keyof MessageTypes, U extends keyof MessageTypes>(opts: CreateMessageOpts<T, U>): TbDEXMessage<T> {
+  const id = typeid(opts.type)
+  const threadId = opts.last?.threadId ?? id.toString()
+  const parentId = opts.last?.id.toString() ?? undefined
+  const {type, body} = opts
+
+  return {
+    id,
+    threadId,
+    parentId,
+    to          : opts.to,
+    from        : opts.from,
+    createdTime : new Date().toISOString(),
+    type,
+    body,
+  }
+}
+
+type LastMessageOpts<T extends keyof MessageTypes> = {
+last: TbDEXMessage<T>
+}
+
+export type CreateMessageSmartOpts<T extends keyof MessageTypes, U extends keyof MessageTypes> = XOR<NewThreadOpts, LastMessageOpts<U>> & {
+  type: T,
+  body: MessageType<T>
+}
+
+export function createMessageSmart<T extends keyof MessageTypes, U extends keyof MessageTypes>(opts: CreateMessageSmartOpts<T, U>): TbDEXMessage<T> {
+  const id = typeid(opts.type)
+  const threadId = opts.last?.threadId ?? id.toString()
+  const parentId = opts.last?.id.toString() ?? undefined
+  const {type, body} = opts
+
+  let from: string
+  let to: string
+  switch (opts.type) {
+  case 'close':
+    from = opts.from
+    to = opts.to
+    break
+  case 'orderstatus':
+    from = opts.last.from
+    to = opts.last.to
+    break
+  case 'quote':
+    from = opts.last.to
+    to = opts.last.from
+    break
+  case 'rfq':
+    from = opts.from
+    to = opts.to
+    break
+  }
+
+  return {
+    id,
+    threadId,
+    parentId,
+    from,
+    to,
+    createdTime: new Date().toISOString(),
+    type,
+    body,
+  }
 }
 
 export type CreateMessageOpts2<T extends keyof MessageTypes, U extends keyof MessageTypes, X extends keyof MessageTypes> =
@@ -26,7 +91,7 @@ XOR<NewThreadOpts, ReplyMessageOpts<U>> & {
   last?: TbDEXMessage<X>
 }
 
-export function createMessage<T extends keyof MessageTypes, U extends keyof MessageTypes, X extends keyof MessageTypes>(opts: CreateMessageOpts2<T, U, X>): TbDEXMessage<T> {
+export function createMessage2<T extends keyof MessageTypes, U extends keyof MessageTypes, X extends keyof MessageTypes>(opts: CreateMessageOpts2<T, U, X>): TbDEXMessage<T> {
   const id = typeid(opts.type)
   const threadId = opts.last?.threadId ?? id.toString()
   const parentId = opts.last?.id.toString() ?? undefined
--- js/tests/builders.spec.ts
+++ js/tests/builders.spec.ts
@@ -1,27 +1,12 @@
-import { type Quote, type Rfq, type TbDEXMessage } from '../src/types.js'
+import { Close, type Quote, type Rfq, type TbDEXMessage } from '../src/types.js'
 
 import { expect } from 'chai'
 import { typeid } from 'typeid-js'
-import { createMessage } from '../src/builders.js'
+import { createMessageDumb, createMessageSmart } from '../src/builders.js'
 
 describe('messages builders', () => {
   it('can build an rfq', () => {
-    const rfq: Rfq = {
-      offeringId          : typeid('offering'),
-      quoteAmountSubunits : '1000',
-      kycProof            : 'fake-jwt',
-      payinMethod         : {
-        kind: 'APPLE_PAY',
-      },
-      payoutMethod: {
-        kind           : 'BTC_ADDRESS',
-        paymentDetails : {
-          btcAddress: 'abcd123'
-        }
-      }
-    }
-
-    const actual = createMessage({
+    const actual = createMessageDumb({
       to   : 'alice-did',
       from : 'pfi-did',
       type : 'rfq',
@@ -32,52 +17,98 @@ describe('messages builders', () => {
     expect(actual.parentId).to.be.undefined
   })
   it('builds the expected message for an existing thread', () => {
-    const rfq: Rfq = {
-      offeringId          : typeid('offering'),
-      quoteAmountSubunits : '1000',
-      kycProof            : 'fake-jwt',
-      payinMethod         : {
-        kind: 'APPLE_PAY',
-      },
-      payoutMethod: {
-        kind           : 'BTC_ADDRESS',
-        paymentDetails : {
-          btcAddress: 'abcd123'
-        }
-      }
-    }
-
-    const rfqMessage: TbDEXMessage<'rfq'> = createMessage({
+    const rfqMessage: TbDEXMessage<'rfq'> = createMessageDumb({
       to   : 'pfi-did',
       from : 'alice-did',
       type : 'rfq',
       body : rfq
     })
 
-    const quote: Quote = {
-      expiryTime : new Date().toISOString(),
-      base       : {
-        currencyCode   : 'BTC',
-        amountSubunits : '33333'
-      },
-      quote: {
-        currencyCode   : 'USD',
-        amountSubunits : '1000',
-        feeSubunits    : '100'
-      },
-      paymentInstructions: { payin: { link: 'fake.link.com' } },
-    }
-
-    const { from, to, threadId, parentId, id } = createMessage({
+    // in the dumb version, to/from are always required, even when responding to an existing thread
+    const quoteMessage = createMessageDumb({
       last : rfqMessage,
+      to   : rfqMessage.from,
+      from : rfqMessage.to,
       type : 'quote',
       body : quote
     })
 
-    expect(from).to.equal(rfqMessage.to)
-    expect(to).to.equal(rfqMessage.from)
-    expect(threadId).to.equal(rfqMessage.threadId)
-    expect(parentId).to.equal(rfqMessage.id.toString())
-    expect(id.toString()).to.contain('quote_')
+    expect(quoteMessage.from).to.equal(rfqMessage.to)
+    expect(quoteMessage.to).to.equal(rfqMessage.from)
+    expect(quoteMessage.threadId).to.equal(rfqMessage.threadId)
+    expect(quoteMessage.parentId).to.equal(rfqMessage.id.toString())
+    expect(quoteMessage.id.toString()).to.contain('quote_')
+
+    // simulates PFI closing a quote after it's been sent
+    const closeMessage: TbDEXMessage<'close'> = createMessageDumb( {
+      last : quoteMessage,
+      type : 'close',
+      body : close,
+      to   : quoteMessage.to,
+      from : quoteMessage.from
+    })
+    expect(closeMessage.to).to.equal(quoteMessage.to)
+    expect(closeMessage.from).to.equal(quoteMessage.from)
+  })
+
+  it('shows how create message smart works', () => {
+    const rfqMessage: TbDEXMessage<'rfq'> = createMessageSmart({
+      to   : 'pfi-did',
+      from : 'alice-did',
+      type : 'rfq',
+      body : rfq
+    })
+
+    const quoteMessage: TbDEXMessage<'quote'> = createMessageSmart( {
+      type : 'quote',
+      body : quote,
+      last : rfqMessage
+    })
+
+    expect(quoteMessage.from).to.equal(rfqMessage.to)
+    expect(quoteMessage.to).to.equal(rfqMessage.from)
+
+    // no intellisense warning that you need to pass to/from for a close message
+    const closeMessage: TbDEXMessage<'close'> = createMessageSmart( {
+      type : 'close',
+      body : close,
+      last : quoteMessage
+    })
+
+    // fails because we didnt pass to/from, so it is undefined on closeMessage
+    expect(closeMessage.to).to.equal(quoteMessage.to)
   })
 })
+
+const rfq: Rfq = {
+  offeringId          : typeid('offering'),
+  quoteAmountSubunits : '1000',
+  kycProof            : 'fake-jwt',
+  payinMethod         : {
+    kind: 'APPLE_PAY',
+  },
+  payoutMethod: {
+    kind           : 'BTC_ADDRESS',
+    paymentDetails : {
+      btcAddress: 'abcd123'
+    }
+  }
+}
+
+const quote: Quote = {
+  expiryTime : new Date().toISOString(),
+  base       : {
+    currencyCode   : 'BTC',
+    amountSubunits : '33333'
+  },
+  quote: {
+    currencyCode   : 'USD',
+    amountSubunits : '1000',
+    feeSubunits    : '100'
+  },
+  paymentInstructions: { payin: { link: 'fake.link.com' } },
+}
+
+const close: Close = {
+  reason: 'test'
+}
