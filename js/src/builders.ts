9e519eeff9ab656cd8dbefa881f83b81e02061bc
Jiyoon Koo <jiyoon@squareup.com>
2023-07-17 22:56:53 -0700

    introducing reply type. wip. test will probably fail

--- js/src/builders.ts
+++ js/src/builders.ts
@@ -10,12 +10,23 @@ type LastMessageOpts<T extends keyof MessageTypes> = {
   last: TbDEXMessage<T>
 }
 
+type ReplyMessageOpts<T extends keyof MessageTypes> = {
+  reply: TbDEXMessage<T>
+}
+
 export type CreateMessageOpts<T extends keyof MessageTypes, U extends keyof MessageTypes> = XOR<NewThreadOpts, LastMessageOpts<U>> & {
   type: T,
   body: MessageType<T>,
 }
 
-export function createMessage<T extends keyof MessageTypes, U extends keyof MessageTypes>(opts: CreateMessageOpts<T, U>): TbDEXMessage<T> {
+export type CreateMessageOpts2<T extends keyof MessageTypes, U extends keyof MessageTypes, X extends keyof MessageTypes> =
+XOR<NewThreadOpts, ReplyMessageOpts<U>> & {
+  type: T,
+  body: MessageType<T>,
+  last?: TbDEXMessage<X>
+}
+
+export function createMessage<T extends keyof MessageTypes, U extends keyof MessageTypes, X extends keyof MessageTypes>(opts: CreateMessageOpts2<T, U, X>): TbDEXMessage<T> {
   const id = typeid(opts.type)
   const threadId = opts.last?.threadId ?? id.toString()
   const parentId = opts.last?.id.toString() ?? undefined
@@ -25,8 +36,8 @@ export function createMessage<T extends keyof MessageTypes, U extends keyof Mess
     id,
     threadId,
     parentId,
-    to          : opts.from ?? opts.last.from,
-    from        : opts.to ?? opts.last.to,
+    to          : opts.reply.from ?? opts.to,
+    from        : opts.reply.to ?? opts.from,
     createdTime : new Date().toISOString(),
     type,
     body,
