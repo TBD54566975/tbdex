118cc1d8322ce0633504db0a92c94ed7d938be07
Moe Jangda <moe@tbd.email>
2023-08-24 01:53:28 -0500

    add pending tests

---
+++ js/tests/resource.spec.ts
@@ -0,0 +1,23 @@
+import { expect } from 'chai'
+
+describe('Resource', () => {
+  describe('create', () => {
+    xit('needs tests')
+  })
+
+  describe('parse', () => {
+    xit('needs tests')
+  })
+
+  describe('validate', () => {
+    xit('needs tests')
+  })
+
+  describe('verify', () => {
+    xit('needs tests')
+  })
+
+  describe('sign', () => {
+    xit('needs tests')
+  })
+})
