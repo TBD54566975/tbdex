3e2c9dfa6cb69bda48b58e9e21ad78b12121ed45
Moe Jangda <moe@tbd.email>
2023-09-01 22:38:37 -0700

    fix `Order` class

--- js/src/message-kinds/order.ts
+++ js/src/message-kinds/order.ts
@@ -1,14 +1,14 @@
 import { OrderModel, MessageKind } from '../types.js'
 
 export class Order {
-  /** a set of valid Message kinds that can come after a close */
+  /** a set of valid Message kinds that can come after an Order */
   readonly validNext = new Set<MessageKind>(['orderstatus'])
   readonly kind: MessageKind = 'order'
 
   readonly data: OrderModel
 
-  constructor(closeData: OrderModel) {
-    this.data = closeData
+  constructor(orderData: OrderModel = {}) {
+    this.data = orderData
   }
 
   toJSON() {
